dfdade50b6cd205b2150e9e2091380a41b41e0c3
erlendak <erlendak@mail.uio.no>
2019-09-04 16:54:49 +0200

    Started working on a fast special Thomas algorithm

---
+++ fast_special_thomas_algorithm.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+n = 10000
+_x0 = 0 # Initial condition
+_x1 = 1 # Initial condition
+h = (_x1 - _x0) / n # Step size
+_x = np.linspace(_x0,_x1,n)
+
+
+b =  100* np.exp(-10*_x)*h**2
+_b = np.copy(b)
+
+d    = np.zeros(n) + 2
+_d   = np.copy(d)
+
+c    = np.zeros(n) - 1
+c[-1] = 0 # Initial condition
+_c   = np.copy(c)
+
+a    =  np.zeros(n) - 1
+a[0] = 0 # Initial conditon
+
+
+
+_c[0] = _c[0]/_d[0]
+_b[0] = b[0]/d[0]
+for i in range(1,n):
+    _c[i] = (     c[i]  /  ( d[i] - (a[i]*_c[i-1])  )     )
+    _b[i] = ( (b[i] -(a[i]*_b[i-1] ) )   / (d[i]- (a[i]*_c[i-1]))   ) #_d[i] - (_b[i-1] * a[i-1] )  )
+
+
+_b_ = np.copy(_b)
+
+N = 9
+
+for i in range(1,n):
+    _b_[N-i] = _b_[N-i] - (_c[N-i]*_b_[N-i+1] )
+plt.plot(_x, _b_, label = "Approximation")
+#plt.plot(_x,( 1-(1-np.exp(-10))*_x - np.exp(-10*_x) ), label = "Analytical")
+#plt.plot(_x,( 100*np.exp(-10*_x) ), label = "test")
+plt.legend()
+plt.show()
