39547067d7037f8685179ee2c503078e6b3d982f
Erlendak <Erlendak@mail.uio.no>
2019-09-07 00:13:53 +0200

    Fixed the intial condtion and timing buges.

--- thomas_algorithm.py
+++ thomas_algorithm.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-start_time = time.time()
 
-n = 10000
+n = 10
 _x0 = 0 # Initial condition
 _x1 = 1 # Initial condition
 h = (_x1 - _x0) / n # Step size
@@ -12,6 +11,8 @@ _x = np.linspace(_x0,_x1,n)
 
 b =  100* np.exp(-10*_x)*h**2
 _b = np.copy(b)
+
+
 
 d    = np.zeros(n) + 2
 _d   = np.copy(d)
@@ -22,12 +23,13 @@ _c   = np.copy(c)
 
 a    =  np.zeros(n) - 1
 a[0] = 0 # Initial conditon
+a[1] = 0
 
+start_time = time.time()
 
-
-_c[0] = _c[0]/_d[0]
-_b[0] = b[0]/d[0]
-for i in range(1,n):
+_c[1] = _c[1]/_d[1]
+_b[1] = b[1]/d[1]
+for i in range(2,n):
     _c[i] = (     c[i]  /  ( d[i] - (a[i]*_c[i-1])  )     )
     _b[i] = ( (b[i] -(a[i]*_b[i-1] ) )   / (d[i]- (a[i]*_c[i-1]))   ) #_d[i] - (_b[i-1] * a[i-1] )  )
 
@@ -40,7 +42,7 @@ for i in range(1,n):
     v[N-i] = v[N-i] - (_c[N-i]*v[N-i+1] )
 print("--- %s seconds ---" % (time.time() - start_time))
 
-plt.plot(_x, v, label = "Approximation")
+plt.plot(_x, v, label = "Approximation" , marker = '+')
 
 u = 1-(1-np.exp(-10))*_x - np.exp(-10*_x)
 plt.plot(_x,u, label = "Analytical")
