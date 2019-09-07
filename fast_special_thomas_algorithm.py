4e03b659b9053bdad554cb3d85f3baf3ba0303cf
erlendak <erlendak@mail.uio.no>
2019-09-07 20:04:59 +0200

    Made the LU decomposition callable.

--- fast_special_thomas_algorithm.py
+++ fast_special_thomas_algorithm.py
@@ -1,49 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+class FastSpecialThomasAlorithm():
+    """¨
+    Approximationm for the potential from a charge,
+    using a system of differential equation and a augmented matrix
+    to calculate the potential, using a faster specialized form of
+    the thomas algorithm and a backwards substitution.
+    """
+    def __init__(self, n):
+        self.n = n
+        _x0 = 0 # Initial condition
+        _x1 = 1 # Initial condition
+        h = (_x1 - _x0) / n # Step size
+        self.x = np.linspace(_x0,_x1,n)
 
-n = int(input("How many steps in the approximation: n = ") )
-_x0 = 0 # Initial condition
-_x1 = 1 # Initial condition
-h = (_x1 - _x0) / n # Step size
-_x = np.linspace(_x0,_x1,n)
+        d    = 2
+        ac0 = -1
 
-d    = 2
-ac0 = -1
+        b =  100* np.exp(-10*self.x)*h**2
+        _b = np.copy(b)
 
-b =  100* np.exp(-10*_x)*h**2
-_b = np.copy(b)
+        _c = np.zeros(n-2) - 1
+        start_time = time.time()
 
-_c    = np.zeros(n-2) - 1
-
-
-start_time = time.time()
-
-_b[0] = 0
-_c[0] = _c[0]/d
-_b[1] = b[1]/d
-for i in range(2,n-1):
-    _c[i-1] = (     ac0  /  ( d - (ac0*_c[i-2])  )     )
-    _b[i] = ( (b[i] -(ac0*_b[i-1] ) )   / (d- (ac0*_c[i-2]))   ) #d[i] - (_b[i-1] * a[i-1] )  )
+        _b[0] = 0
+        _c[0] = _c[0]/d
+        _b[1] = b[1]/d
+        for i in range(2,n-1):
+            _c[i-1] = (     ac0  /  ( d - (ac0*_c[i-2])  )     )
+            _b[i] = ( (b[i] -(ac0*_b[i-1] ) )   / (d- (ac0*_c[i-2]))   ) #d[i] - (_b[i-1] * a[i-1] )  )
 
 
-N = n-1
-for i in range(1,n-1):
-    _b[N-i] = _b[N-i] - (_c[(N-1)-i]*_b[(N+1)-i] )
+        #N = (n-1)
+        for i in range(1,n-1):
+            _b[(n-1)-i] = _b[(n-1)-i] - (_c[(n-2)-i]*_b[n-i] )
+        self.t = (time.time() - start_time)
+        self.v = _b
+        self.u = 1-(1-np.exp(-10))*self.x - np.exp(-10*self.x)
 
 
-print("--- %s seconds ---" % (time.time() - start_time))
+    def plot(self):
+        plt.plot(self.x, self.v , label = "Approximation" , marker = '+')
 
-u = 1-(1-np.exp(-10))*_x - np.exp(-10*_x)
-plt.plot(_x,u, label = "Analytical" , marker = '+')
-plt.plot(_x, _b, label = "Approximation", marker = '+')
-plt.legend()
-plt.grid()
-plt.xlabel("Distance ; Meter",size=15)
-plt.ylabel("Potential ; Volt",size=15)
-plt.title("Potential from charge\nFast special method ",size=15)
-plt.show()
+        plt.plot(self.x , self.u , label = "Analytical")
+        plt.legend()
+        plt.grid()
+        plt.xlabel("Distance ; Meter",size=15)
+        plt.ylabel("Potential ; Volt",size=15)
+        plt.title("Potential from charge\nFast special method ",size=15)
+        plt.show()
 
-Error = np.log(  abs( (_b[1:]-u[1:])/u[1:] )  )
-print(np.max(Error))
-print(np.max(abs(abs(_b) - abs(u))))
+    def error(self):
+        error =np.max( np.log(  abs( (self.v[1:]-self.u[1:])/self.u[1:] )  ))
+        return(error)
+
+    def _error(self):
+        error= np.max(abs(abs(self.v) - abs(self.u)))
+        return(error)
+
+    def time(self):
+        return(self.t)
+
+
+
+
+if __name__ == '__main__':
+    n = int(input("How many steps in the approximation: n = ") )
+    test = FastSpecialThomasAlorithm(n)
+    test.plot()
+    print("--- %s seconds ---" % (test.time()))
+    print(test.error())
+    print(test._error())
--- lu_decomposition.py
+++ lu_decomposition.py
@@ -2,37 +2,70 @@ import numpy as np
 import scipy.linalg as sa
 import matplotlib.pyplot as plt
 import time
+class LUDecomposition():
+    """¨
+    Approximationm for the potential from a charge,
+    using a system of differential equation and LU decomposition and a
+    backwards substitution to calculate the potential from the charge to
+    1 meter away from the charge.
+    """
+    def __init__(self, n):
+        self.n = n
+        _x0 = 0 # Initial condition
+        _x1 = 1 # Initial condition
+        h = (_x1 - _x0) / n # Step size
+        self.x = np.linspace(_x0,_x1,n)
+        b =  100* np.exp(-10*self.x)*h**2
+        _b = np.copy(b)
+        d    = np.diag( np.zeros(n-2) + 2 ) # Diagonal elements
+        c    = np.diag((np.zeros(n-3) - 1 ), 1)# Upperdiagonal elements
+        a    = np.diag( (np.zeros(n-3) - 1) , -1) # Lowerdiagonal elements
+        A = a + d + c # Tridiagonal matrix
+        start_time = time.time()
+        _lu = sa.lu(A)
+        _lu = sa.lu_factor(A)
+        v = sa.lu_solve(_lu, b[1:-1])
+        v = np.insert(v,0,0 )
+        self.v = np.append(v,0 )
+        self.t = (time.time() - start_time)
+        self.u = 1-(1-np.exp(-10))*self.x - np.exp(-10*self.x)
 
-n = int(input("How many steps in the approximation: n = ") )
-_x0 = 0 # Initial condition
-_x1 = 1 # Initial condition
-h = (_x1 - _x0) / n # Step size
-_x = np.linspace(_x0,_x1,n)
+    def plot(self):
+        plt.plot(self.x, self.v , label = "Approximation" , marker = '+')
 
+        plt.plot(self.x , self.u , label = "Analytical")
+        plt.legend()
+        plt.grid()
+        plt.xlabel("Distance ; Meter",size=15)
+        plt.ylabel("Potential ; Volt",size=15)
+        plt.title("Potential from charge\n LU decomposition",size=15)
+        plt.show()
 
-b =  100* np.exp(-10*_x)*h**2
-_b = np.copy(b)
-#b[1] = 0
-d    = np.diag( np.zeros(n-2) + 2 ) # Diagonal elements
-c    = np.diag((np.zeros(n-3) - 1 ), 1)# Upperdiagonal elements
-a    = np.diag( (np.zeros(n-3) - 1) , -1) # Lowerdiagonal elements
-A = a + d + c # Tridiagonal matrix
-#print(A)
-_lu = sa.lu(A)
-_lu = sa.lu_factor(A)
-v = sa.lu_solve(_lu, b[1:-1])
-#print(solutition)
-#print(sp.__version__)
+    def error(self):
+        error =np.max( np.log(  abs( (self.v[1:]-self.u[1:])/self.u[1:] )  ))
+        return(error)
 
-#print(_lu)
+    def _error(self):
+        error= np.max(abs(abs(self.v) - abs(self.u)))
+        return(error)
 
-#start_time = time.time()
+    def time(self):
+        return(self.t)
 
 
 
-#print("--- %s seconds ---" % (time.time() - start_time))
-v = np.insert(v,0,0 )
-v = np.append(v,0 )
+
+if __name__ == '__main__':
+    n = int(input("How many steps in the approximation: n = ") )
+    test = LUDecomposition(n)
+    test.plot()
+    print("--- %s seconds ---" % (test.time()))
+    print(test.error())
+    print(test._error())
+
+
+
+"""
 plt.plot(_x,v , label = "Approximation" , marker = '+')
 
 u = 1-(1-np.exp(-10))*_x - np.exp(-10*_x)
@@ -47,3 +80,4 @@ plt.show()
 Error = np.log(  abs( (v[1:]-u[1:])/u[1:] )  )
 print(np.max(Error))
 print(np.max( abs(abs(v) - abs(u) )))
+"""
